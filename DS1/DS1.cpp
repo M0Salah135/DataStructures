0cf07ea2f17cdf7a6afc0e547d229de2fa0dc332
muhammed3ly <40716171+muhammed3ly@users.noreply.github.com>
2020-05-08 14:18:55 +0200

    Stack Initialize

--- DS1/DS1.cpp
+++ DS1/DS1.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 #include "List.h"
 #include<assert.h>
+#include <stack>
 using namespace std;
 
 int main()
 {
-	List l;
-	l.append(1);
-	l.append(2);
-	l.append(3);
-	l.append(4);
-	l.print();
-	l.insert(-1, 100);
-	l.print();
+	//push
+	//pop
+	//top
+	//empty
+	//size
 	return 0;
 }
